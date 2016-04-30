79d1bc42291f8dbc89c714e9633f438603e03b7c
Sơn Phạm <sonpxvn@gmail.com>
2016-04-30 01:25:53 -0700

    update admin

--- application/controllers/admincp.php
+++ application/controllers/admincp.php
@@ -25,32 +25,31 @@ class Admincp extends CI_Controller {
         }
     }
 
-    public function details() {
-        $this->load->view('home/index');
-    }
+ 
 
     function login() {
         if ($this->session->userdata('admin_id') == null) {
-            if (isset($_REQUEST['go'])) {
+            if (isset($_REQUEST['loginBtt'])) {
                 $username = $this->input->post('email', true);
                 $password = $this->input->post('password', true);
                 $this->load->model('user_model');
-                $result = $this->user_model->authen($username, $password);
-                if (empty($result)) {
+                $data = array(
+                    'user'=>$username,
+                    'password'=>$password,
+                );
+                $result = $this->user_model->authen($data); 
+                if ($result == 0) {
                     redirect('admincp/login/error');
                 } else {
                     $session_user = array(
-                        'admin_id' => $result['user_id'],
-                        'admin_name' => $result['user_name'],
-                        'admin_type' => $result['user_type'],
-                        'admin_active' => $result['user_active'],
+                        'admin_id' =>$result,
+                        'admin_name' => $username, 
                     );
                     $this->session->set_userdata($session_user);
                     redirect('admincp/index');
                 }
-            }
-            $this->lang->load("login", "vietnam");
-            $this->load->view('backend/login');
+            } 
+            $this->load->view('admin/login');
         } else {
             redirect('admincp/index');
         }
--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -42,7 +42,7 @@
                         <h3 class="panel-title">Please Sign In</h3>
                     </div>
                     <div class="panel-body">
-                        <form role="form">
+                         <?php echo form_open_multipart('admincp/login',array('role'=>"form")); ?>
                             <fieldset>
                                 <div class="form-group">
                                     <input class="form-control" placeholder="E-mail" name="email" type="email" autofocus>
@@ -55,10 +55,10 @@
                                         <input name="remember" type="checkbox" value="Remember Me">Remember Me
                                     </label>
                                 </div>
-                                <!-- Change this to a button or input when using this as a form -->
-                                <a href="index.html" class="btn btn-lg btn-success btn-block">Login</a>
+                                <!-- Change this to a button or input when using this as a form --> 
+                                <input type="submit" name="loginBtt" class="btn btn-lg btn-success btn-block" value="Đăng nhập"/>
                             </fieldset>
-                        </form>
+                        <?php echo form_close(); ?>
                     </div>
                 </div>
             </div>
